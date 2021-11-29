fd533c547a9e1df71dcd0a6a84e7db9cad8a4187
mario pereira <marioscpereira@gmail.com>
2021-11-29 18:22:14 +0000

    added routes and updated models with active records associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :restaurants do
+    resources :reviews, only: %i[index new create]
+    resources :restaurant_vibes, only: %i[create destroy]
+  end
+  resources :collections do
+    resources :restaurant_collections, only: %i[create destroy]
+  end
+  resources :vibes, only: %i[index]
 end
