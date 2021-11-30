d75b36d5db5488bd75a460b1a7cc8cdd41c4e9b5
Kristina Balciunaite <xinaspace@gmail.com>
2021-11-30 14:56:05 +0000

    created seeds with faker and unsplash

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Pulling out seeds"
+Restaurant.delete_all
+
+puts "Planting new ones"
+
+16.times do
+  Restaurant.create(
+  name: Faker::Restaurant.name,
+  cuisine: Faker::Restaurant.type,
+  address: Faker::Address.street_address,
+  website: Faker::Internet.url,
+  phone_number: Faker::PhoneNumber.cell_phone,
+  menu: rand(1..10),
+  price_range: "$" * rand(1..4),
+  lat: Faker::Address.latitude,
+  lon: Faker::Address.longitude,
+  image: 'https://source.unsplash.com/random'
+)
+end
+
+puts "#{Restaurant.count} restaurant seeds planted"
