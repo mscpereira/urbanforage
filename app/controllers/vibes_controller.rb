a7287bcf7383af4b4255b96fde50e5e33eced389
paulo araujo <paulo.pp.araujo@gmail.com>
2021-11-30 16:43:25 +0000

    button with link to restaurants path

--- app/controllers/vibes_controller.rb
+++ app/controllers/vibes_controller.rb
@@ -1,5 +1,6 @@
 class VibesController < ApplicationController
   def index
     @vibes = Vibe.all
+    @photos = ["https://images.unsplash.com/photo-1494346480775-936a9f0d0877?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=816&q=80"]
   end
 end
