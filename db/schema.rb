d28fa72f6eee4689c89e09af42be722b76b97b49
Kristina Balciunaite <xinaspace@gmail.com>
2021-11-30 15:15:33 +0000

    gemfiel and schema deets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_29_170724) do
+ActiveRecord::Schema.define(version: 2021_11_30_144024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2021_11_29_170724) do
     t.float "lon"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "reviews", force: :cascade do |t|
