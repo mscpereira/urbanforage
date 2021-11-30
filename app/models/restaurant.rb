930e671310393d0b37a5994ab8895d6cccc37f35
Alexandro Nussbaumer <90007596+alexandroNussbaumer@users.noreply.github.com>
2021-11-30 15:18:55 +0000

    restaurant controller + show css + cloudinary

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,6 @@ class Restaurant < ApplicationRecord
   has_many :reviews
   has_many :restaurant_vibes
   has_many :vibes, through: :restaurant_vibes
+  has_many_attached :photos
+  validates :photos, presence: true
 end
