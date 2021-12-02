24999dc20a317fbbee066a98527d64c62171a49a
Kristina Balciunaite <xinaspace@gmail.com>
2021-12-02 13:03:24 +0000

    created seeds and added photo model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_11_30_173723) do
-
-
+ActiveRecord::Schema.define(version: 2021_12_02_115108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +50,14 @@ ActiveRecord::Schema.define(version: 2021_11_30_173723) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_collections_on_user_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string "url"
+    t.bigint "restaurant_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_photos_on_restaurant_id"
   end
 
   create_table "restaurant_collections", force: :cascade do |t|
@@ -121,6 +126,7 @@ ActiveRecord::Schema.define(version: 2021_11_30_173723) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "collections", "users"
+  add_foreign_key "photos", "restaurants"
   add_foreign_key "restaurant_collections", "collections"
   add_foreign_key "restaurant_collections", "restaurants"
   add_foreign_key "restaurant_vibes", "restaurants"
