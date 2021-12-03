def4693500ea3cad0f4cb038466a2ede915ae573
mario pereira <marioscpereira@gmail.com>
2021-12-03 15:27:10 +0000

    prepared pundit for collections. We dont need policies for other models. Fixed the links and after signin and signup redirect - added store_location_for in both index and show.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,10 @@
 class RestaurantsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
   before_action :find_restaurant, only: %i[show]
 
   def index
+    store_location_for(:user, restaurants_path)
+
     if params.keys.count == 2
       flash[:alert] = "You need to select at least one vibe!"
       redirect_to :vibes
@@ -18,6 +21,7 @@ class RestaurantsController < ApplicationController
   end
 
   def show
+    store_location_for(:user, restaurant_path)
     @restaurant = Restaurant.find(params[:id])
     @review = Review.new
     @markers = {
