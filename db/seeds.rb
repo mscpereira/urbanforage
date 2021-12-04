61c6ba3b9a93834ad0e2aebc2c8534739a526b66
Kristina Balciunaite <xinaspace@gmail.com>
2021-12-04 15:15:55 +0000

    added directions to each seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -68,298 +68,303 @@ end
 
 
 
-# # GENERATING UP MELODIC VIBES
+# GENERATING UP MELODIC VIBES
 
-# puts "-----"
-# puts "Tuning up some melodic restaurant seeds"
+puts "-----"
+puts "Tuning up some melodic restaurant seeds"
 
-# melodic_vibe = Vibe.create(name: 'melodic')
+melodic_vibe = Vibe.create(name: 'melodic')
 
-# melodic_params = {
-#   engine: "google_maps",
-#   q: "restaurants+with+live+music",
-#   ll: "@38.7223,-9.1393,15.1z",
-#   type: "search",
-#   api_key: ENV['GOOGLE_API_KEY']
-# }
+melodic_params = {
+  engine: "google_maps",
+  q: "restaurants+with+live+music",
+  ll: "@38.7223,-9.1393,15.1z",
+  type: "search",
+  api_key: ENV['GOOGLE_API_KEY']
+}
 
-# melodic_search = GoogleSearch.new(melodic_params)
-# melodic_results = melodic_search.get_hash[:local_results]
+melodic_search = GoogleSearch.new(melodic_params)
+melodic_results = melodic_search.get_hash[:local_results]
 
-# melodic_results.each do |result|
-#   melodic_photo_params = {
-#     engine: "google_maps_photos",
-#     q: "",
-#     data_id: result[:data_id],
-#     api_key: ENV['GOOGLE_API_KEY']
-#   }
-#   melodic_photo_search = GoogleSearch.new(melodic_photo_params)
-#   melodic_photos = melodic_photo_search.get_hash[:photos]
+melodic_results.each do |result|
+  melodic_photo_params = {
+    engine: "google_maps_photos",
+    q: "",
+    data_id: result[:data_id],
+    api_key: ENV['GOOGLE_API_KEY']
+  }
+  melodic_photo_search = GoogleSearch.new(melodic_photo_params)
+  melodic_photos = melodic_photo_search.get_hash[:photos]
 
-#   melodic_restaurant = Restaurant.create!(
-#   name: result[:title],
-#   cuisine: result[:type],
-#   address: result[:address],
-#   website: result[:website],
-#   phone_number: result[:phone],
-#   menu: rand(1..10),
-#   price_range: result[:price],
-#   latitude: result[:gps_coordinates][:latitude],
-#   longitude: result[:gps_coordinates][:longitude],
-#   image: melodic_photos[0][:image],
-#   description: result[:description]
-# )
+  melodic_restaurant = Restaurant.create!(
+  name: result[:title],
+  cuisine: result[:type],
+  address: result[:address],
+  website: result[:website],
+  phone_number: result[:phone],
+  menu: rand(1..10),
+  price_range: result[:price],
+  latitude: result[:gps_coordinates][:latitude],
+  longitude: result[:gps_coordinates][:longitude],
+  image: melodic_photos[0][:image],
+  description: result[:description],
+  directions: "https://www.google.com/maps/search/?api=1&query=#{result[:gps_coordinates][:latitude]}%2C#{result[:gps_coordinates][:longitude]}&query_place_id=#{result[:place_id]}"
+)
 
-#   melodic_photos_array = []
-#   melodic_photos.first(8).each { |image| melodic_photos_array << image[:image] }
+  melodic_photos_array = []
+  melodic_photos.first(8).each { |image| melodic_photos_array << image[:image] }
 
-#   melodic_photos_array.each do |url|
-#     Photo.create(
-#       url: url,
-#       restaurant_id: melodic_restaurant[:id]
-#     )
-#   end
+  melodic_photos_array.each do |url|
+    Photo.create(
+      url: url,
+      restaurant_id: melodic_restaurant[:id]
+    )
+  end
 
-#   RestaurantVibe.create(
-#     vibe_id: melodic_vibe[:id],
-#     restaurant_id: melodic_restaurant[:id]
-#   )
-# end
+  RestaurantVibe.create(
+    vibe_id: melodic_vibe[:id],
+    restaurant_id: melodic_restaurant[:id]
+  )
+end
 
-# # GENERATING COZY VIBES
-# puts "-----"
-# puts "Snuggling up some cozy restaurant seeds"
+# GENERATING COZY VIBES
+puts "-----"
+puts "Snuggling up some cozy restaurant seeds"
 
-# cozy_vibe = Vibe.create(name: 'cozy')
+cozy_vibe = Vibe.create(name: 'cozy')
 
-# cozy_params = {
-#   engine: "google_maps",
-#   q: "restaurant+simple+snug",
-#   ll: "@38.7223,-9.1393,15.1z",
-#   type: "search",
-#   api_key: ENV['GOOGLE_API_KEY']
-# }
+cozy_params = {
+  engine: "google_maps",
+  q: "restaurant+simple+snug",
+  ll: "@38.7223,-9.1393,15.1z",
+  type: "search",
+  api_key: ENV['GOOGLE_API_KEY']
+}
 
-# cozy_search = GoogleSearch.new(cozy_params)
-# cozy_results = cozy_search.get_hash[:local_results]
+cozy_search = GoogleSearch.new(cozy_params)
+cozy_results = cozy_search.get_hash[:local_results]
 
-# cozy_results.each do |result|
-#   cozy_photo_params = {
-#     engine: "google_maps_photos",
-#     q: "",
-#     data_id: result[:data_id],
-#     api_key: ENV['GOOGLE_API_KEY']
-#   }
-#   cozy_photo_search = GoogleSearch.new(cozy_photo_params)
-#   cozy_photos = cozy_photo_search.get_hash[:photos]
+cozy_results.each do |result|
+  cozy_photo_params = {
+    engine: "google_maps_photos",
+    q: "",
+    data_id: result[:data_id],
+    api_key: ENV['GOOGLE_API_KEY']
+  }
+  cozy_photo_search = GoogleSearch.new(cozy_photo_params)
+  cozy_photos = cozy_photo_search.get_hash[:photos]
 
-#   cozy_restaurant = Restaurant.create!(
-#   name: result[:title],
-#   cuisine: result[:type],
-#   address: result[:address],
-#   website: result[:website],
-#   phone_number: result[:phone],
-#   menu: rand(1..10),
-#   price_range: result[:price],
-#   latitude: result[:gps_coordinates][:latitude],
-#   longitude: result[:gps_coordinates][:longitude],
-#   image: cozy_photos[0][:image],
-#   description: result[:description]
-# )
+  cozy_restaurant = Restaurant.create!(
+  name: result[:title],
+  cuisine: result[:type],
+  address: result[:address],
+  website: result[:website],
+  phone_number: result[:phone],
+  menu: rand(1..10),
+  price_range: result[:price],
+  latitude: result[:gps_coordinates][:latitude],
+  longitude: result[:gps_coordinates][:longitude],
+  image: cozy_photos[0][:image],
+  description: result[:description],
+  directions: "https://www.google.com/maps/search/?api=1&query=#{result[:gps_coordinates][:latitude]}%2C#{result[:gps_coordinates][:longitude]}&query_place_id=#{result[:place_id]}"
+)
 
-#   cozy_photos_array = []
-#   cozy_photos.first(8).each { |image| cozy_photos_array << image[:image] }
+  cozy_photos_array = []
+  cozy_photos.first(8).each { |image| cozy_photos_array << image[:image] }
 
-#   cozy_photos_array.each do |url|
-#     Photo.create(
-#       url: url,
-#       restaurant_id: cozy_restaurant[:id]
-#     )
-#   end
+  cozy_photos_array.each do |url|
+    Photo.create(
+      url: url,
+      restaurant_id: cozy_restaurant[:id]
+    )
+  end
 
-#   RestaurantVibe.create(
-#     vibe_id: cozy_vibe[:id],
-#     restaurant_id: cozy_restaurant[:id]
-#   )
-# end
-
-
-
-# # GENERATING EARTHY VIBES
-
-# puts "-----"
-# puts "Planting some earthy, organic restaurant seeds"
-
-# earthy_vibe = Vibe.create(name: 'earthy')
-
-# earthy_params = {
-#   engine: "google_maps",
-#   q: "restaurant+plants+interior+decor",
-#   ll: "@38.7223,-9.1393,15.1z",
-#   type: "search",
-#   api_key: ENV['GOOGLE_API_KEY']
-# }
-
-# earthy_search = GoogleSearch.new(earthy_params)
-# earthy_results = earthy_search.get_hash[:local_results]
-
-# earthy_results.each do |result|
-#   earthy_photo_params = {
-#     engine: "google_maps_photos",
-#     q: "",
-#     data_id: result[:data_id],
-#     api_key: ENV['GOOGLE_API_KEY']
-#   }
-#   earthy_photo_search = GoogleSearch.new(earthy_photo_params)
-#   earthy_photos = earthy_photo_search.get_hash[:photos]
-
-#   earthy_restaurant = Restaurant.create!(
-#   name: result[:title],
-#   cuisine: result[:type],
-#   address: result[:address],
-#   website: result[:website],
-#   phone_number: result[:phone],
-#   menu: rand(1..10),
-#   price_range: result[:price],
-#   latitude: result[:gps_coordinates][:latitude],
-#   longitude: result[:gps_coordinates][:longitude],
-#   image: earthy_photos[0][:image],
-#   description: result[:description]
-# )
-
-#   earthy_photos_array = []
-#   earthy_photos.first(8).each { |image| earthy_photos_array << image[:image] }
-
-#   earthy_photos_array.each do |url|
-#     Photo.create(
-#       url: url,
-#       restaurant_id: earthy_restaurant[:id]
-#     )
-#   end
-
-#   RestaurantVibe.create(
-#     vibe_id: earthy_vibe[:id],
-#     restaurant_id: earthy_restaurant[:id]
-#   )
-# end
+  RestaurantVibe.create(
+    vibe_id: cozy_vibe[:id],
+    restaurant_id: cozy_restaurant[:id]
+  )
+end
 
 
-# # GENERATING MINIMALIST VIBES
-# puts "-----"
-# puts "Asking interior designer for some minimalist restaurant seeds"
 
-# minimalist_vibe = Vibe.create(name: 'minimalist')
+# GENERATING EARTHY VIBES
 
-# minimalist_params = {
-#   engine: "google_maps",
-#   q: "restaurant+modern+design+decor",
-#   ll: "@38.7223,-9.1393,15.1z",
-#   type: "search",
-#   api_key: ENV['GOOGLE_API_KEY']
-# }
+puts "-----"
+puts "Planting some earthy, organic restaurant seeds"
 
-# minimalist_search = GoogleSearch.new(minimalist_params)
-# minimalist_results = minimalist_search.get_hash[:local_results]
+earthy_vibe = Vibe.create(name: 'earthy')
 
-# minimalist_results.each do |result|
-#   minimalist_photo_params = {
-#     engine: "google_maps_photos",
-#     q: "",
-#     data_id: result[:data_id],
-#     api_key: ENV['GOOGLE_API_KEY']
-#   }
-#   minimalist_photo_search = GoogleSearch.new(minimalist_photo_params)
-#   minimalist_photos = minimalist_photo_search.get_hash[:photos]
+earthy_params = {
+  engine: "google_maps",
+  q: "restaurant+plants+interior+decor",
+  ll: "@38.7223,-9.1393,15.1z",
+  type: "search",
+  api_key: ENV['GOOGLE_API_KEY']
+}
 
-#   minimalist_restaurant = Restaurant.create!(
-#   name: result[:title],
-#   cuisine: result[:type],
-#   address: result[:address],
-#   website: result[:website],
-#   phone_number: result[:phone],
-#   menu: rand(1..10),
-#   price_range: result[:price],
-#   latitude: result[:gps_coordinates][:latitude],
-#   longitude: result[:gps_coordinates][:longitude],
-#   image: minimalist_photos[0][:image],
-#   description: result[:description]
-# )
+earthy_search = GoogleSearch.new(earthy_params)
+earthy_results = earthy_search.get_hash[:local_results]
 
-#   minimalist_photos_array = []
-#   minimalist_photos.first(8).each { |image| minimalist_photos_array << image[:image] }
+earthy_results.each do |result|
+  earthy_photo_params = {
+    engine: "google_maps_photos",
+    q: "",
+    data_id: result[:data_id],
+    api_key: ENV['GOOGLE_API_KEY']
+  }
+  earthy_photo_search = GoogleSearch.new(earthy_photo_params)
+  earthy_photos = earthy_photo_search.get_hash[:photos]
 
-#   minimalist_photos_array.each do |url|
-#     Photo.create(
-#       url: url,
-#       restaurant_id: minimalist_restaurant[:id]
-#     )
-#   end
+  earthy_restaurant = Restaurant.create!(
+  name: result[:title],
+  cuisine: result[:type],
+  address: result[:address],
+  website: result[:website],
+  phone_number: result[:phone],
+  menu: rand(1..10),
+  price_range: result[:price],
+  latitude: result[:gps_coordinates][:latitude],
+  longitude: result[:gps_coordinates][:longitude],
+  image: earthy_photos[0][:image],
+  description: result[:description],
+  directions: "https://www.google.com/maps/search/?api=1&query=#{result[:gps_coordinates][:latitude]}%2C#{result[:gps_coordinates][:longitude]}&query_place_id=#{result[:place_id]}"
+)
 
-#   RestaurantVibe.create(
-#     vibe_id: minimalist_vibe[:id],
-#     restaurant_id: minimalist_restaurant[:id]
-#   )
-# end
+  earthy_photos_array = []
+  earthy_photos.first(8).each { |image| earthy_photos_array << image[:image] }
 
-# # GENERATING ALTERNATIVE VIBES
-# puts "-----"
-# puts "Thrifting some alternative restaurant seeds"
+  earthy_photos_array.each do |url|
+    Photo.create(
+      url: url,
+      restaurant_id: earthy_restaurant[:id]
+    )
+  end
 
-# alternative_vibe = Vibe.create(name: 'alternative')
+  RestaurantVibe.create(
+    vibe_id: earthy_vibe[:id],
+    restaurant_id: earthy_restaurant[:id]
+  )
+end
 
-# alternative_params = {
-#   engine: "google_maps",
-#   q: "restaurant+interesting+concept+hipster",
-#   ll: "@38.7223,-9.1393,15.1z",
-#   type: "search",
-#   api_key: ENV['GOOGLE_API_KEY']
-# }
 
-# alternative_search = GoogleSearch.new(alternative_params)
-# alternative_results = alternative_search.get_hash[:local_results]
+# GENERATING MINIMALIST VIBES
+puts "-----"
+puts "Asking interior designer for some minimalist restaurant seeds"
 
-# alternative_results.each do |result|
-#   alternative_photo_params = {
-#     engine: "google_maps_photos",
-#     q: "",
-#     data_id: result[:data_id],
-#     api_key: ENV['GOOGLE_API_KEY']
-#   }
-#   alternative_photo_search = GoogleSearch.new(alternative_photo_params)
-#   alternative_photos = alternative_photo_search.get_hash[:photos]
+minimalist_vibe = Vibe.create(name: 'minimalist')
 
-#   alternative_restaurant = Restaurant.create!(
-#   name: result[:title],
-#   cuisine: result[:type],
-#   address: result[:address],
-#   website: result[:website],
-#   phone_number: result[:phone],
-#   menu: rand(1..10),
-#   price_range: result[:price],
-#   latitude: result[:gps_coordinates][:latitude],
-#   longitude: result[:gps_coordinates][:longitude],
-#   image: alternative_photos[0][:image],
-#   description: result[:description]
-# )
+minimalist_params = {
+  engine: "google_maps",
+  q: "restaurant+modern+design+decor",
+  ll: "@38.7223,-9.1393,15.1z",
+  type: "search",
+  api_key: ENV['GOOGLE_API_KEY']
+}
 
-#   alternative_photos_array = []
-#   alternative_photos.first(8).each { |image| alternative_photos_array << image[:image] }
+minimalist_search = GoogleSearch.new(minimalist_params)
+minimalist_results = minimalist_search.get_hash[:local_results]
 
-#   alternative_photos_array.each do |url|
-#     Photo.create(
-#       url: url,
-#       restaurant_id: alternative_restaurant[:id]
-#     )
-#   end
+minimalist_results.each do |result|
+  minimalist_photo_params = {
+    engine: "google_maps_photos",
+    q: "",
+    data_id: result[:data_id],
+    api_key: ENV['GOOGLE_API_KEY']
+  }
+  minimalist_photo_search = GoogleSearch.new(minimalist_photo_params)
+  minimalist_photos = minimalist_photo_search.get_hash[:photos]
 
-#   RestaurantVibe.create(
-#     vibe_id: alternative_vibe[:id],
-#     restaurant_id: alternative_restaurant[:id]
-#   )
-# end
+  minimalist_restaurant = Restaurant.create!(
+  name: result[:title],
+  cuisine: result[:type],
+  address: result[:address],
+  website: result[:website],
+  phone_number: result[:phone],
+  menu: rand(1..10),
+  price_range: result[:price],
+  latitude: result[:gps_coordinates][:latitude],
+  longitude: result[:gps_coordinates][:longitude],
+  image: minimalist_photos[0][:image],
+  description: result[:description],
+  directions: "https://www.google.com/maps/search/?api=1&query=#{result[:gps_coordinates][:latitude]}%2C#{result[:gps_coordinates][:longitude]}&query_place_id=#{result[:place_id]}"
+)
 
-# puts "------------------------"
-# puts "#{Restaurant.count} total restaurant seeds planted, with #{Photo.count} photos attached"
+  minimalist_photos_array = []
+  minimalist_photos.first(8).each { |image| minimalist_photos_array << image[:image] }
+
+  minimalist_photos_array.each do |url|
+    Photo.create(
+      url: url,
+      restaurant_id: minimalist_restaurant[:id]
+    )
+  end
+
+  RestaurantVibe.create(
+    vibe_id: minimalist_vibe[:id],
+    restaurant_id: minimalist_restaurant[:id]
+  )
+end
+
+# GENERATING ALTERNATIVE VIBES
+puts "-----"
+puts "Thrifting some alternative restaurant seeds"
+
+alternative_vibe = Vibe.create(name: 'alternative')
+
+alternative_params = {
+  engine: "google_maps",
+  q: "restaurant+interesting+concept+hipster",
+  ll: "@38.7223,-9.1393,15.1z",
+  type: "search",
+  api_key: ENV['GOOGLE_API_KEY']
+}
+
+alternative_search = GoogleSearch.new(alternative_params)
+alternative_results = alternative_search.get_hash[:local_results]
+
+alternative_results.each do |result|
+  alternative_photo_params = {
+    engine: "google_maps_photos",
+    q: "",
+    data_id: result[:data_id],
+    api_key: ENV['GOOGLE_API_KEY']
+  }
+  alternative_photo_search = GoogleSearch.new(alternative_photo_params)
+  alternative_photos = alternative_photo_search.get_hash[:photos]
+
+  alternative_restaurant = Restaurant.create!(
+  name: result[:title],
+  cuisine: result[:type],
+  address: result[:address],
+  website: result[:website],
+  phone_number: result[:phone],
+  menu: rand(1..10),
+  price_range: result[:price],
+  latitude: result[:gps_coordinates][:latitude],
+  longitude: result[:gps_coordinates][:longitude],
+  image: alternative_photos[0][:image],
+  description: result[:description],
+  directions: "https://www.google.com/maps/search/?api=1&query=#{result[:gps_coordinates][:latitude]}%2C#{result[:gps_coordinates][:longitude]}&query_place_id=#{result[:place_id]}"
+)
+
+  alternative_photos_array = []
+  alternative_photos.first(8).each { |image| alternative_photos_array << image[:image] }
+
+  alternative_photos_array.each do |url|
+    Photo.create(
+      url: url,
+      restaurant_id: alternative_restaurant[:id]
+    )
+  end
+
+  RestaurantVibe.create(
+    vibe_id: alternative_vibe[:id],
+    restaurant_id: alternative_restaurant[:id]
+  )
+end
+
+puts "------------------------"
+puts "#{Restaurant.count} total restaurant seeds planted, with #{Photo.count} photos attached"
 
 
 
