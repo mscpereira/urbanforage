7718e402d68c01a2b335db02fff73d4c96dfcc72
Kristina Balciunaite <xinaspace@gmail.com>
2021-12-07 12:31:35 +0000

    collection actions and pages

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,4 +1,5 @@
 class CollectionsController < ApplicationController
+  before_action :set_collection, only: %i[show edit update destroy]
 
   def index
     @users = User.all
@@ -7,5 +8,49 @@ class CollectionsController < ApplicationController
     @user_collections = Collection.where(user_id: current_user.id)
     @friends_collections = Collection.where(user_id: current_user.friends)
     # @restaurant_collections = RestaurantCollection.all
+  end
+
+  def new
+    @collection = Collection.new
+  end
+
+  def create
+    @collection = Collection.new(collection_params)
+    @collection.user = current_user
+    if @collection.save
+      redirect_to collections_path(@collection)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @collection.update(collection_params)
+    if @collection.update(collection_params)
+      redirect_to collection_path(@collection)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @collection.destroy
+    redirect_to collections_path
+  end
+
+  private
+
+  def set_collection
+    @collection = Collection.find(params[:id])
+  end
+
+  def collection_params
+    params.require(:collection).permit(:name, :description)
   end
 end
