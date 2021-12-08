58a7bd3fe007a55760b86ff405ca31a6bf091e45
mario pereira <marioscpereira@gmail.com>
2021-12-08 16:19:51 +0000

    reviews show button not fixed

--- app/javascript/plugins/read_more.js
+++ app/javascript/plugins/read_more.js
@@ -1,24 +1,26 @@
 // const text = document.querySelector('.text-overflow');
 
 const readMore = () => {
-  const reviewPara = document.querySelector('.teste');
-  const allBtn = document.querySelectorAll('.btn-overflow');
-  allBtn.forEach((btn) => {
-    btn.addEventListener("click", (event) => {
-      console.log("hey");
-      if (reviewPara) {
-        reviewPara.classList.toggle("review-para");
+  const reviewPara = document.getElementById('table');
+  if (reviewPara) {
+    const elementHeight = document.querySelectorAll(".review-para")[2].offsetHeight
+    const allBtn = document.querySelectorAll('.btn-overflow');
+    allBtn.forEach((btn) => {
+      if (elementHeight > 38) {
+        btn.classList.add("disabled-button")
+      }
+      btn.addEventListener("click", (event) => {
+        // console.log(event.path[1].firstElementChild);
+        const textElement = event.path[1].firstElementChild;
+        textElement.classList.toggle("review-para");
         if (event.target.innerHTML === "Show more...") {
           event.target.innerHTML = "Show less...";
         } else {
           event.target.innerHTML = "Show more...";
         }
-      }
-
-  })
-
-  });
+      });
+    });
+  }
 }
-
 
 export { readMore }
